de5d21dbb2882eeddd9daae195ec90b05fb60cc0
Eugene <sshabrovivivi@gmail.com>
2022-12-04 15:16:50 +0400

    до реализации БД

---
+++ check_post.php
@@ -0,0 +1,103 @@
+<?php
+    $title = "Пользователь";
+    $headline ="Ваш профиль";
+    require_once "./header.php";
+
+ 
+  //присвоение переменных для дальнейшего использования в html части
+$username = $_POST['username'];
+if( trim($username) == "")
+{
+    echo " Ошибка: Поле Имя не должно быть пустым"; //выводит сообщение если поле имя заполнено пробелами
+}
+else{
+
+  
+  $email = $_POST['email'];         //почта
+  $aboutMe = $_POST['message'];    //о себе
+  $identity = $_POST['select']; 
+ 
+   
+  
+  //сохраняем аватар в папку на сервере, его имя будет храниться в БД
+  //if( isset($_FILES) && $_FILES['avatar']['name'] == 0){ echo ' Файл загружен  ';}
+  
+  $destination_dir = getcwd().DIRECTORY_SEPARATOR . "usersImages\\";//$destiation_dir = "\usersImages";
+  echo $destination_dir;echo ' ';
+  $target_path = $destination_dir . basename( $_FILES["avatar"]["name"]);
+  echo $target_path;  //echo __DIR__.$destiation_dir;
+  move_uploaded_file($_FILES['avatar']['tmp_name'], $target_path );
+
+  switch($_POST['sex'] )           //пол пользователя
+  {
+    case 'option1':
+        $sex = "мужской";
+        break;
+    case 'option2':
+        $sex = "женский";
+         break;
+    default:
+         $sex = "не указан";
+        break;
+  }
+
+    $langString;                   // чекбоксы языков в 1 строке
+    if(empty($_POST['langCheck'])) 
+    {
+      $langString = "Вы не выбрали ни одного языка.";
+    } 
+   else
+    {
+      $N = count($_POST['langCheck']);
+
+      if($N==1){  //echo("Вы выбрали $N язык: ");
+          $langString .="Изучает $N язык: ".$_POST['langCheck'][0];
+        }
+        else {
+          // echo("Вы выбрали $N языка: ");
+          $langString .="Изучает $N языка: ";
+            for($i=0; $i < $N; $i++)
+            {
+            $langString .= $_POST['langCheck'][$i] . " " ;
+            }
+        }
+    }
+  }
+  ?>
+
+  
+<body>
+
+<!-- container mt-2 -->
+<div class="container">  
+        <div class="row">
+<div class=" col-lg-8 col-md-8 ">
+  
+    <h4>О пользователе</h4> 
+    <table class="table table-th-block">
+      <tbody>
+       <tr><td class="active">Зарегистрирован:</td><td>ЕЩЕ НЕ СДЕЛАЛ</td></tr>
+       <tr><td class="active">Имя:</td><td><?php echo $username?></td></tr>
+       <tr><td class="active">Почта:</td><td><?php echo $email?></td></tr>
+       <tr><td class="active">Пароль (md5):</td><td><?php echo md5($password) ?></td></tr>
+       <tr><td class="active">Пол:</td><td><?php echo $sex?></td></tr>
+       <tr><td class="active">Языки:</td><td><?php echo $langString?></td></tr>
+       <tr><td class="active">Идентичность:</td><td><?php echo $identity?></td></tr>
+       <tr><td class="active">О себе:</td><td><?php echo $aboutMe?></td></tr>
+     </tbody>
+    </table>
+ </div>
+ 
+  <div class="  col-md-4 my-5 " >
+  <img class = "img-fluid " src="https://www.meme-arsenal.com/memes/a5365dfac79e08ce52538cfcb9124d3f.jpg">
+  </div> 
+</div> 
+</div>
+ 
+
+ 
+ 
+
+
+</body>
+  
---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+    $title = "Contact us";
+    $headline = "Свяжитесь с нами";
+    require_once "./header.php";
+?>
+
+
+<div class="container mt-2">
+    <form enctype="multipart/form-data" action="check_post.php" method="post">
+
+        <div class="mb-2">
+            <label for="inputName">Введите имя</label>
+            <input required type="text" class="form-control" id="inputName" name="username"  placeholder="имя" >
+        </div>
+
+        <div class="mb-2">
+            <label for="inputEmail">Введите email</label>
+            <input required type="email" class="form-control" id="inputEmail"  name="email"  placeholder="почта@gmail.com">
+        </div>
+
+        <div class="mb-2">
+            <label for="inputPassword">Введите пароль</label>
+            <input required type="password" class="form-control" id="inputPassword" name="password" placeholder="пароль" >
+        </div>
+
+        <div class="mb-2">
+       
+            <label for="inputFile" class="form-label">Выберите фотографию (файл) </label>
+            <input type="file" class="form-control-file"  id="inputFile" name ="avatar">
+        </div>
+
+        <div class="mb-2">
+            <textarea name="message" placeholder="Напишите о себе" class="form-control"></textarea><br/>
+        </div>
+        
+        <label class="form-check-label">Ваш пол</label>         <label class="form-check-label space120 ">Выберите языки, которые вы знаете</label><br>
+        
+        <div class="form-check form-check-inline" >
+            <input class="form-check-input" type="radio" name="sex"  value="option1">
+            <label class="form-check-label" for="male">М</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="sex"  value="option2">
+            <label class="form-check-label" for="female">Ж</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="sex"  value="option3" >
+            <label class="form-check-label" for="it">Не знаю</label>
+        </div>
+
+        
+
+        <div class="form-check form-check-inline ml-5">
+            <input class="form-check-input " type="checkbox" id="PHPcheck" name="langCheck[]" value="PHP">
+            <label class="form-check-label" for="PHPcheck">PHP</label>
+        </div>
+        <div class="form-check form-check-inline ">
+            <input class="form-check-input" type="checkbox" id="JScheck" name="langCheck[]" value="JS">
+            <label class="form-check-label" for="JScheck">JS</label>
+        </div>
+        <div class="form-check form-check-inline ">
+            <input class="form-check-input" type="checkbox" id="Rubycheck" name="langCheck[]" value="Ruby">
+            <label class="form-check-label" for="Rubycheck">Ruby</label>
+        </div>
+       
+
+        <div class="form-group">
+            <label for="select1">Вы:</label>
+            <select class="form-control" name="select" id="select1">
+                <option>Человек</option>
+                <option>Волк</option>
+                <option>Кофеин</option>
+                <option>Робот</option>
+            </select>
+        </div>
+       <div class="d-flex justify-content-end">
+            <input type="reset" class="btn btn-primary me-md-2" name="reset" placeholder="введите пароль" class="form-control">
+            <input type="submit" value="Отправить" class="btn btn-success ml-2 ">
+        </div>
+    </form>
+</div>
+
+
+
+
+
+<?php
+    require_once "./footer.php";   
+?>
